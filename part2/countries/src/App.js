38d5da226798079f663b975bae7ed6c018262aca
sravya2502 <74001042+sravya2502@users.noreply.github.com>
2021-06-07 16:53:47 +0530

    Add files via upload

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,1 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import FindCountry from './component/FindCountry';
+import CountryList from './component/CountryList';
 
+const App = () => {
+  // List of all countries
+  const [countries, setCountries] = useState([]);
+
+  // Country to be found
+  const [country, setCountry] = useState('');
+
+  // Fetch countries from API
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+      });
+  }, []);
+  
+
+  return (
+    <div>
+      {
+        countries.length ?
+        <>
+          <FindCountry country={country} setCountry={setCountry} />
+          { country && <CountryList country={country} countries={countries} setCountry={setCountry} /> }
+        </>
+        :
+        <p>Loading application...</p>
+      }
+    </div>
+  );
+};
+
+export default App;
